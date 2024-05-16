22ca847adcb48f9c7945c9a7f7ac6ff9bef95bbd
johnsonevanna <59804169+johnsonevanna@users.noreply.github.com>
2024-05-16 06:55:31 +0000

    Completed

--- _02_02/Tickets.java
+++ _02_02/Tickets.java
@@ -8,5 +8,24 @@ public class Tickets {
   public Tickets(){
 
   }
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+  public void setPrice(double price){
+    this.price = price;
+  }
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
+  public String getDestination(){
+    return destination;
+  }
   
+  public double getPrice() {
+    return price;
+  }
+  
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 }
